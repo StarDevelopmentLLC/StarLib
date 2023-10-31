775f58511991d56483af52702e3b0fbb7eae2873
Firestar311 <slaterandrew0398@gmail.com>
2023-10-30 22:41:29 -0500

    Moved the Registry class and made it so that the key is also generic.

---
+++ src/main/java/com/stardevllc/starlib/registry/Registry.java
@@ -0,0 +1,76 @@
+package com.stardevllc.starlib.registry;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Registry<K, V> implements Iterable<V> {
+    protected final Map<K, V> objects = new HashMap<>();
+    protected final Function<K, K> keyNormalizer;
+
+    public Registry(Map<K, V> initialObjects, Function<K, K> keyNormalizer) {
+        if (initialObjects != null && !initialObjects.isEmpty()) {
+            objects.putAll(initialObjects);
+        }
+        this.keyNormalizer = keyNormalizer;
+    }
+
+    public Registry(Function<K, K> keyNormalizer) {
+        this(null, keyNormalizer);
+    }
+    
+    public Registry() {
+        this(null, null);
+    }
+    
+    public void register(K key, V object) {
+        if (keyNormalizer != null) {
+            key = keyNormalizer.apply(key);
+        }
+        this.objects.put(key, object);
+    }
+
+    public void registerAll(Map<K, V> map) {
+        map.forEach(this::register);
+    }
+
+    public V get(K key) {
+        if (keyNormalizer != null) {
+            key = keyNormalizer.apply(key);
+        }
+        return objects.get(key);
+    }
+
+    public Map<K, V> getObjects() {
+        return new HashMap<>(this.objects);
+    }
+
+    public boolean contains(K key) {
+        if (keyNormalizer != null) {
+            key = keyNormalizer.apply(key);
+        }
+        return objects.containsKey(key);
+    }
+
+    public void deregister(K key) {
+        if (keyNormalizer != null) {
+            key = keyNormalizer.apply(key);
+        }
+        objects.remove(key);
+    }
+
+    @Override
+    public Iterator<V> iterator() {
+        return new ArrayList<>(objects.values()).iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super V> action) {
+        new ArrayList<>(objects.values()).forEach(action);
+    }
+
+    @Override
+    public Spliterator<V> spliterator() {
+        return new ArrayList<>(objects.values()).spliterator();
+    }
+}
