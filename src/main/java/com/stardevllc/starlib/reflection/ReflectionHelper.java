152f71289e118205d1f71b81af34107c7c130dc4
Firestar311 <slaterandrew0398@gmail.com>
2023-10-30 22:34:49 -0500

    Added the two reflection class from the old star lib

---
+++ src/main/java/com/stardevllc/starlib/reflection/ReflectionHelper.java
@@ -0,0 +1,100 @@
+package com.stardevllc.starlib.reflection;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * A class to make it easier to recursively get friends and methods of a class
+ */
+public final class ReflectionHelper {
+    
+    /**
+     * Recursively gets a method of a class from itself or it's parent class(es)
+     * @param clazz The base class to get the method from
+     * @param name The name of the method - Case Sensitive
+     * @param parameters The parameters of the method
+     * @return The method or null if it does not exist in the class heirarchy
+     */
+    public static Method getClassMethod(Class<?> clazz, String name, Class<?>... parameters) {
+        try {
+            return clazz.getDeclaredMethod(name, parameters);
+        } catch (NoSuchMethodException e) {}
+
+        return getClassMethod(clazz.getSuperclass(), name, parameters);
+    }
+
+    /**
+     * Recursively gets all methods of a class and of it's parent class(es)
+     * @param clazz The class to get the methods from
+     * @return The set of methods. This should never be null, if it is, please report as a bug
+     */
+    public static Set<Method> getClassMethods(Class<?> clazz) {
+        Set<Method> methods = new LinkedHashSet<>(Arrays.asList(clazz.getDeclaredMethods()));
+        if (clazz.getSuperclass() != null && clazz.getSuperclass() != Object.class) {
+            getClassMethods(clazz.getSuperclass(), methods);
+        }
+        return methods;
+    }
+
+    /**
+     * The is a helper method for the other one and gets all methods from the provided class and the parent class(es)
+     * @param clazz The class to get the methods from
+     * @param methods The set of existing methods. This set must be mutable to work
+     */
+    public static void getClassMethods(Class<?> clazz, Set<Method> methods) {
+        if (methods == null) {
+            methods = new LinkedHashSet<>();
+        }
+        
+        methods.addAll(Arrays.asList(clazz.getDeclaredMethods()));
+        if (clazz.getSuperclass() != null) {
+            getClassMethods(clazz.getSuperclass(), methods);
+        }
+    }
+
+    /**
+     * Recursively gets a field from a class and it's parent class(es)
+     * @param clazz The class to get the field from
+     * @param name The name of the field - Case Sensitive
+     * @return The field or null if it does not exist
+     */
+    public static Field getClassField(Class<?> clazz, String name) {
+        try {
+            return clazz.getDeclaredField(name);
+        } catch (NoSuchFieldException e) {}
+
+        return getClassField(clazz.getSuperclass(), name);
+    }
+
+    /**
+     * Recursively gets all fields from a class and it's parent class(es)
+     * @param clazz The class to get the fields from
+     * @return The set of fields of the class and it's parent class(es). This should never be null, if it is, please report as a bug
+     */
+    public static Set<Field> getClassFields(Class<?> clazz) {
+        Set<Field> fields = new LinkedHashSet<>(Arrays.asList(clazz.getDeclaredFields()));
+        if (clazz.getSuperclass() != null) {
+            getClassFields(clazz.getSuperclass(), fields);
+        }
+        return fields;
+    }
+
+    /**
+     * The is a helper method for the other one and gets all fields from the provided class and the parent class(es)
+     * @param clazz The class to get the fields from
+     * @param fields The set of existing fields. This set must be mutable to work
+     */
+    public static void getClassFields(Class<?> clazz, Set<Field> fields) {
+        if (fields == null) {
+            fields = new LinkedHashSet<>();
+        }
+
+        fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+        if (clazz.getSuperclass() != null) {
+            getClassFields(clazz.getSuperclass(), fields);
+        }
+    }
+}
---
+++ src/main/java/com/stardevllc/starlib/reflection/URLClassLoaderAccess.java
@@ -0,0 +1,138 @@
+package com.stardevllc.starlib.reflection;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Collection;
+
+/**
+ * Allows access to a URLClassLoader's addUrl method. <br>
+ * Use the create() method to initialize<br>
+ * Mainly intended for use with Spigot's plugin class loaders
+ */
+public abstract class URLClassLoaderAccess {
+
+    /**
+     * Creates an access instance for the given classLoader
+     * @param classLoader The class loader
+     * @return The access implementation
+     */
+    public static URLClassLoaderAccess create(URLClassLoader classLoader) {
+        if (classLoader == null) {
+            return null;
+        }
+        if (Reflection.isSupported()) {
+            return new Reflection(classLoader);
+        } else if (Unsafe.isSupported()) {
+            return new Unsafe(classLoader);
+        } else {
+            return Noop.INSTANCE;
+        }
+    }
+
+    private final URLClassLoader classLoader;
+
+    protected URLClassLoaderAccess(URLClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    public abstract void addURL(URL url);
+
+    private static class Reflection extends URLClassLoaderAccess {
+        private static final Method ADD_URL_METHOD;
+
+        static {
+            Method addUrlMethod;
+            try {
+                addUrlMethod = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+                addUrlMethod.setAccessible(true);
+            } catch (Exception e) {
+                addUrlMethod = null;
+            }
+            ADD_URL_METHOD = addUrlMethod;
+        }
+
+        private static boolean isSupported() {
+            return ADD_URL_METHOD != null;
+        }
+
+        Reflection(URLClassLoader classLoader) {
+            super(classLoader);
+        }
+
+        @Override
+        public void addURL(URL url) {
+            try {
+                ADD_URL_METHOD.invoke(super.classLoader, url);
+            } catch (ReflectiveOperationException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static class Unsafe extends URLClassLoaderAccess {
+        private static final sun.misc.Unsafe UNSAFE;
+
+        static {
+            sun.misc.Unsafe unsafe;
+            try {
+                Field unsafeField = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
+                unsafeField.setAccessible(true);
+                unsafe = (sun.misc.Unsafe) unsafeField.get(null);
+            } catch (Throwable t) {
+                unsafe = null;
+            }
+            UNSAFE = unsafe;
+        }
+
+        private static boolean isSupported() {
+            return UNSAFE != null;
+        }
+
+        private final Collection<URL> unopenedURLs;
+        private final Collection<URL> pathURLs;
+
+        Unsafe(URLClassLoader classLoader) {
+            super(classLoader);
+
+            Collection<URL> unopenedURLs;
+            Collection<URL> pathURLs;
+            try {
+                Object ucp = fetchField(URLClassLoader.class, classLoader, "ucp");
+                unopenedURLs = (Collection<URL>) fetchField(ucp.getClass(), ucp, "unopenedUrls");
+                pathURLs = (Collection<URL>) fetchField(ucp.getClass(), ucp, "path");
+            } catch (Throwable e) {
+                unopenedURLs = null;
+                pathURLs = null;
+            }
+            this.unopenedURLs = unopenedURLs;
+            this.pathURLs = pathURLs;
+        }
+
+        private static Object fetchField(final Class<?> clazz, final Object object, final String name) throws NoSuchFieldException {
+            Field field = clazz.getDeclaredField(name);
+            long offset = UNSAFE.objectFieldOffset(field);
+            return UNSAFE.getObject(object, offset);
+        }
+
+        @Override
+        public void addURL(URL url) {
+            this.unopenedURLs.add(url);
+            this.pathURLs.add(url);
+        }
+    }
+
+    private static class Noop extends URLClassLoaderAccess {
+        private static final Noop INSTANCE = new Noop();
+
+        private Noop() {
+            super(null);
+        }
+
+        @Override
+        public void addURL(URL url) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
