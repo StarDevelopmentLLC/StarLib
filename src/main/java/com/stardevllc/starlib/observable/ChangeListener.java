c6642986670519f9982b1892ad403d43f712b7f5
Firestar311 <slaterandrew0398@gmail.com>
2024-03-31 15:48:41 -0600

    Removed the old observable stuff

--- src/main/java/com/stardevllc/starlib/observable/ChangeListener.java
+++
@@ -1,6 +0,0 @@
-package com.stardevllc.starlib.observable;
-
-@FunctionalInterface
-public interface ChangeListener<T> {
-    void onChange(ObservableValue<T> observable, T oldObject, T newObject);
-}
