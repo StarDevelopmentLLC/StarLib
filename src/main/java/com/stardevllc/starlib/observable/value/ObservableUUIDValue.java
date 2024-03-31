470e92d0389333eee0487d6985e3038012b4eabc
Firestar311 <slaterandrew0398@gmail.com>
2024-03-31 17:04:42 -0600

    Forgot to commit some of the files

---
+++ src/main/java/com/stardevllc/starlib/observable/value/ObservableUUIDValue.java
@@ -0,0 +1,6 @@
+package com.stardevllc.starlib.observable.value;
+
+import java.util.UUID;
+
+public interface ObservableUUIDValue extends ObservableObjectValue<UUID> {
+}
---
+++ src/main/java/com/stardevllc/starlib/observable/writable/WritableUUIDValue.java
@@ -0,0 +1,6 @@
+package com.stardevllc.starlib.observable.writable;
+
+import java.util.UUID;
+
+public interface WritableUUIDValue extends WritableObjectValue<UUID> {
+}
