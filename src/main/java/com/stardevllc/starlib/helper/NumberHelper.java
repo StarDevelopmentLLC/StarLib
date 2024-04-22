7090134e412493009d73ed067b792986e2eec7ae
Firestar311 <slaterandrew0398@gmail.com>
2024-04-22 17:33:27 -0600

    Added a roman numerals utility to NumberHelper

--- src/main/java/com/stardevllc/starlib/helper/NumberHelper.java
+++ src/main/java/com/stardevllc/starlib/helper/NumberHelper.java
@@ -1,7 +1,39 @@
 package com.stardevllc.starlib.helper;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public final class NumberHelper {
     private NumberHelper() {}
+
+    private static final Map<String, Integer> romanNumerals = new LinkedHashMap<>();
+
+    static {
+        romanNumerals.put("M", 1000);
+        romanNumerals.put("CM", 900);
+        romanNumerals.put("D", 500);
+        romanNumerals.put("CD", 400);
+        romanNumerals.put("C", 100);
+        romanNumerals.put("XC", 90);
+        romanNumerals.put("L", 50);
+        romanNumerals.put("XL", 40);
+        romanNumerals.put("X", 10);
+        romanNumerals.put("IX", 9);
+        romanNumerals.put("V", 5);
+        romanNumerals.put("IV", 4);
+        romanNumerals.put("I", 1);
+    }
+
+    public static String romanNumerals(int number) {
+        StringBuilder res = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : romanNumerals.entrySet()) {
+            int matches = number / entry.getValue();
+            res.append(String.valueOf(entry.getKey()).repeat(Math.max(0, matches)));
+            number = number % entry.getValue();
+        }
+        return res.toString();
+    }
+    
     public static int toInt(Object object) {
         if (object instanceof Number) {
             return ((Number) object).intValue();
