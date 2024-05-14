6dd8d6ccad5b45352bea79cde1a59a36791522bb
Firestar311 <slaterandrew0398@gmail.com>
2024-05-14 06:22:25 -0500

    Added proper instance checks of numbers to keep precision

--- numbers/src/main/java/com/stardevllc/starlib/math/Operations.java
+++ numbers/src/main/java/com/stardevllc/starlib/math/Operations.java
@@ -5,8 +5,22 @@ public final class Operations {
         if (number == null) {
             return 0;
         }
+        
+        if (number instanceof Double) {
+            return -number.doubleValue();
+        } else if (number instanceof Float) {
+            return -number.floatValue();
+        } else if (number instanceof Long) {
+            return -number.longValue();
+        } else if (number instanceof Integer) {
+            return -number.intValue();
+        } else if (number instanceof Short) {
+            return -number.shortValue();
+        } else if (number instanceof Byte) {
+            return -number.byteValue();
+        }
 
-        return -number.doubleValue();
+        return 0;
     }
     
     public static Number add(Number left, Number right) {
@@ -14,23 +28,65 @@ public final class Operations {
             return 0;
         }
         
-        return left.doubleValue() + right.doubleValue();
+        if (left instanceof Double || right instanceof Double) {
+            return left.doubleValue() + right.doubleValue();
+        } else if (left instanceof Float || right instanceof Float) {
+            return left.floatValue() + right.floatValue();
+        } else if (left instanceof Long || right instanceof Long) {
+            return left.longValue() + right.longValue();
+        } else if (left instanceof Integer || right instanceof Integer) {
+            return left.intValue() + right.intValue();
+        } else if (left instanceof Short || right instanceof Short) {
+            return left.shortValue() + right.shortValue();
+        } else if (left instanceof Byte || right instanceof Byte) {
+            return left.byteValue() + right.byteValue();
+        }
+        
+        return 0;
     }
     
     public static Number subtract(Number left, Number right) {
         if (left == null || right == null) {
             return 0;
         }
+
+        if (left instanceof Double || right instanceof Double) {
+            return left.doubleValue() - right.doubleValue();
+        } else if (left instanceof Float || right instanceof Float) {
+            return left.floatValue() - right.floatValue();
+        } else if (left instanceof Long || right instanceof Long) {
+            return left.longValue() - right.longValue();
+        } else if (left instanceof Integer || right instanceof Integer) {
+            return left.intValue() - right.intValue();
+        } else if (left instanceof Short || right instanceof Short) {
+            return left.shortValue() - right.shortValue();
+        } else if (left instanceof Byte || right instanceof Byte) {
+            return left.byteValue() - right.byteValue();
+        }
         
-        return left.doubleValue() - right.doubleValue();
+        return 0;
     }
     
     public static Number multiply(Number left, Number right) {
         if (left == null || right == null) {
             return 0;
         }
+
+        if (left instanceof Double || right instanceof Double) {
+            return left.doubleValue() * right.doubleValue();
+        } else if (left instanceof Float || right instanceof Float) {
+            return left.floatValue() * right.floatValue();
+        } else if (left instanceof Long || right instanceof Long) {
+            return left.longValue() * right.longValue();
+        } else if (left instanceof Integer || right instanceof Integer) {
+            return left.intValue() * right.intValue();
+        } else if (left instanceof Short || right instanceof Short) {
+            return left.shortValue() * right.shortValue();
+        } else if (left instanceof Byte || right instanceof Byte) {
+            return left.byteValue() * right.byteValue();
+        }
         
-        return left.doubleValue() * right.doubleValue();
+        return 0;
     }
     
     public static Number divide(Number left, Number right) {
@@ -38,8 +94,22 @@ public final class Operations {
             return 0;
         }
         
-        if (right.doubleValue() == 0.0d) {
+        if (right.doubleValue() == 0.0) {
             return 0;
+        }
+
+        if (left instanceof Double || right instanceof Double) {
+            return left.doubleValue() / right.doubleValue();
+        } else if (left instanceof Float || right instanceof Float) {
+            return left.floatValue() / right.floatValue();
+        } else if (left instanceof Long || right instanceof Long) {
+            return left.longValue() / right.longValue();
+        } else if (left instanceof Integer || right instanceof Integer) {
+            return left.intValue() / right.intValue();
+        } else if (left instanceof Short || right instanceof Short) {
+            return left.shortValue() / right.shortValue();
+        } else if (left instanceof Byte || right instanceof Byte) {
+            return left.byteValue() / right.byteValue();
         }
         
         return left.doubleValue() / right.doubleValue();
