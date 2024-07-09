58d456436f030144d93d04e0b895bf012157b16e
Firestar311 <slaterandrew0398@gmail.com>
2024-07-08 21:05:03 -0500

    Removed some arithmetic methods that werent needed in Duration

--- time/src/main/java/com/stardevllc/starlib/time/Duration.java
+++ time/src/main/java/com/stardevllc/starlib/time/Duration.java
@@ -23,35 +23,6 @@ public class Duration {
         return this;
     }
     
-    public Duration multiply(long factor) {
-        this.time *= factor;
-        return this;
-    }
-    
-    public Duration divide(long divisor) {
-        this.time /= divisor;
-        return this;
-    }
-    
-    public Duration divide(double divisor) {
-        this.time /= (long) divisor;
-        return this;
-    }
-    
-    public Duration pow(long exponent) {
-        this.time = (long) Math.pow(this.time, exponent);
-        return this;
-    }
-    
-    public Duration square() {
-        return pow(2);
-    }
-    
-    public Duration squrt() {
-        this.time = (long) Math.sqrt(this.time);
-        return this;
-    }
-    
     public Duration abs() {
         this.time = Math.abs(this.time);
         return this;
