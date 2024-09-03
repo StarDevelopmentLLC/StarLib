5a44a2dd34ca29f8e58db721250d78ab778d494a
Firestar311 <slaterandrew0398@gmail.com>
2024-09-03 13:20:32 -0500

    Added a default value to the Variable class

--- variable/src/main/java/com/stardevllc/starlib/variable/Variable.java
+++ variable/src/main/java/com/stardevllc/starlib/variable/Variable.java
@@ -9,7 +9,23 @@ public class Variable {
     private final String id;
     private final char openingChar;
     private final char closingChar;
+    private Object defaultValue;
     private String description;
+
+    /**
+     * @param id           This is used for the text between the opening and closing chars, and used for internal identification
+     * @param openingChar  The opening character used. This should be a non-alphanumeric charactar
+     * @param closingChar  The closing character used. This should be a non-alphanumeric character
+     * @param defaultValue The default value of this variable
+     * @param description  Just a description for help or documentation purposes (Like for a config)
+     */
+    public Variable(String id, char openingChar, char closingChar, Object defaultValue, String description) {
+        this.id = id;
+        this.openingChar = openingChar;
+        this.closingChar = closingChar;
+        this.defaultValue = defaultValue;
+        this.description = description;
+    }
 
     /**
      * @param id          This is used for the text between the opening and closing chars, and used for internal identification
@@ -18,10 +34,7 @@ public class Variable {
      * @param description Just a description for help or documentation purposes (Like for a config)
      */
     public Variable(String id, char openingChar, char closingChar, String description) {
-        this.id = id;
-        this.openingChar = openingChar;
-        this.closingChar = closingChar;
-        this.description = description;
+        this(id, openingChar, closingChar, null, description);
     }
 
     /**
@@ -32,7 +45,7 @@ public class Variable {
     public Variable(String id, char openingChar, char closingChar) {
         this(id, openingChar, closingChar, "");
     }
-    
+
     public Variable(String id, VCharSet charSet, String description) {
         this(id, charSet.opening(), charSet.closing(), description);
     }
@@ -60,6 +73,10 @@ public class Variable {
 
     public String getDescription() {
         return description;
+    }
+
+    public Object getDefaultValue() {
+        return defaultValue;
     }
 
     @Override
