b2f3418e1e77249e1349492e8ca3e409c18c6db0
Firestar311 <slaterandrew0398@gmail.com>
2025-06-21 08:23:46 -0500

    Added a newInstance method that filters parameters by type

--- src/main/java/com/stardevllc/helper/ReflectionHelper.java
+++ src/main/java/com/stardevllc/helper/ReflectionHelper.java
@@ -1,7 +1,6 @@
 package com.stardevllc.helper;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 import java.util.*;
 
 public final class ReflectionHelper {
@@ -32,11 +31,63 @@ public final class ReflectionHelper {
         }
     }
     
+    public static <T> Optional<T> newInstance(Class<T> clazz, Object... params) {
+        Class<?>[] paramTypes;
+        if (params != null) {
+            paramTypes = new Class<?>[params.length];
+            for (int i = 0; i < params.length; i++) {
+                paramTypes[i] = params[i].getClass();
+            }
+        } else {
+            paramTypes = new Class<?>[0];
+        }
+        
+        Constructor<?> constructor = getClassConstructor(clazz, paramTypes).orElse(null);
+        if (constructor != null) {
+            constructor.setAccessible(true);
+            try {
+                return Optional.of((T) constructor.newInstance(params));
+            } catch (Exception e) {}
+        }
+        
+        return Optional.empty();
+    }
+    
+    public static Optional<Constructor<?>> getClassConstructor(Class<?> clazz, Class<?>... parameters) {
+        if (parameters == null || parameters.length == 0) {
+            try {
+                return Optional.of(clazz.getDeclaredConstructor());
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        }
+        
+        constructorLoop:
+        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+            constructor.setAccessible(true);
+            Class<?>[] constructorParameterTypes = constructor.getParameterTypes();
+            if (constructorParameterTypes.length != parameters.length) {
+                continue;
+            }
+            
+            for (int i = 0; i < constructorParameterTypes.length; i++) {
+                if (!constructorParameterTypes[i].isAssignableFrom(parameters[i])) {
+                    continue constructorLoop;
+                }
+            }
+            
+            return Optional.of(constructor);
+        }
+        
+        return Optional.empty();
+    }
+    
     public static Method getClassMethod(Class<?> clazz, String name, Class<?>... parameters) {
         try {
             return clazz.getDeclaredMethod(name, parameters);
-        } catch (NoSuchMethodException e) {}
-
+        } catch (NoSuchMethodException e) {
+        }
+        
         return getClassMethod(clazz.getSuperclass(), name, parameters);
     }
     
@@ -62,8 +113,9 @@ public final class ReflectionHelper {
     public static Field getClassField(Class<?> clazz, String name) {
         try {
             return clazz.getDeclaredField(name);
-        } catch (NoSuchFieldException e) {}
-
+        } catch (NoSuchFieldException e) {
+        }
+        
         return getClassField(clazz.getSuperclass(), name);
     }
     
@@ -79,7 +131,7 @@ public final class ReflectionHelper {
         if (fields == null) {
             fields = new LinkedHashSet<>();
         }
-
+        
         fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
         if (clazz.getSuperclass() != null) {
             getClassFields(clazz.getSuperclass(), fields);
