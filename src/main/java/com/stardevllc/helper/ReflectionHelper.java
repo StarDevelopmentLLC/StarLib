c0d35351940cd4ef451a06a7974948e8248d4e3b
Firestar311 <slaterandrew0398@gmail.com>
2025-07-08 15:47:15 -0500

    Added some annotation based reflection methods to ReflectionHelper

--- src/main/java/com/stardevllc/helper/ReflectionHelper.java
+++ src/main/java/com/stardevllc/helper/ReflectionHelper.java
@@ -1,9 +1,100 @@
 package com.stardevllc.helper;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
 import java.util.*;
 
 public final class ReflectionHelper {
+    
+    private static final Map<Method, Annotation> methodAnnotationCache = new HashMap<>();
+    public static <A extends Annotation> A getMethodAnnotation(Method method, Class<A> annotation) {
+        //Check the cache to see if it exists already
+        if (methodAnnotationCache.containsKey(method)) {
+            return (A) methodAnnotationCache.get(method);
+        }
+        
+        //Check to see if the method has the annotation directly
+        if (method.isAnnotationPresent(annotation)) {
+            A a = method.getAnnotation(annotation);
+            methodAnnotationCache.put(method, a);
+            return a;
+        }
+        
+        //Get the declaring class of the method
+        Class<?> declaringClass = method.getDeclaringClass();
+        try {
+            //Search using the recursive way to get the method from the declaring class and it's parents
+            Method annotatedMethod = getAnnotatedMethod(annotation, declaringClass, method.getName(), method.getParameterTypes());
+            //Check to see if the method is null, if not, return the annotation
+            if (annotatedMethod != null) {
+                A a = annotatedMethod.getAnnotation(annotation);
+                methodAnnotationCache.put(method, a);
+                return a;
+            }
+        } catch (Exception e) {}
+        
+        //Return null if nothing is found
+        return null;
+    }
+    
+    public static Method getAnnotatedMethod(Class<? extends Annotation> annotation, Class<?> c, String methodName, Class<?>... parameterTypes) {
+        try {
+            Method method = c.getDeclaredMethod(methodName, parameterTypes);
+            if (method.isAnnotationPresent(annotation)) {
+                return method;
+            }
+            
+            for (Class<?> classInterface : c.getInterfaces()) {
+                Method interfaceMethod = getAnnotatedMethod(annotation, classInterface, methodName, parameterTypes);
+                if (interfaceMethod != null) {
+                    return interfaceMethod;
+                }
+            }
+            
+            return getAnnotatedMethod(annotation, c.getSuperclass(), methodName, parameterTypes);
+        } catch (NoSuchMethodException e) {
+            if (c.getSuperclass() == Object.class) {
+                return null;
+            }
+            
+            return getAnnotatedMethod(annotation, c.getSuperclass(), methodName, parameterTypes);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    
+    private static final Map<Class<?>, Annotation> classAnnotations = new HashMap<>();
+    public static <A extends Annotation> A getClassAnnotation(Class<?> clazz, Class<A> annotation) {
+        if (classAnnotations.containsKey(clazz)) {
+            return (A) classAnnotations.get(clazz);
+        }
+        
+        //Check to see if the class has the annotation directly
+        if (clazz.isAnnotationPresent(annotation)) {
+            A a = clazz.getAnnotation(annotation);
+            classAnnotations.put(clazz, a);
+            return a;
+        }
+        
+        //If the class doesn't have the annotation, look at any interfaces
+        for (Class<?> clazzInterface : clazz.getInterfaces()) {
+            //Check to see if the interface has the annotation directly
+            if (clazzInterface.isAnnotationPresent(annotation)) {
+                A a = clazzInterface.getAnnotation(annotation);
+                classAnnotations.put(clazz, a);
+                return a;
+            }
+        }
+        
+        //If the interfaces don't have the annotation, then look recursively through the hierarchy for it
+        if (clazz.getSuperclass() != Object.class) {
+            return getClassAnnotation(clazz.getSuperclass(), annotation);
+        }
+        
+        //If nothing is found, return null
+        return null;
+    }
+    
     public static Object getValue(Object object, String path) {
         if (!path.contains(".")) {
             return getFieldValue(object, path);
