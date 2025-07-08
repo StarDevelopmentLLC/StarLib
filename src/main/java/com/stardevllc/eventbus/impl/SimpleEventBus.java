6e108a1eca7f1a3e41ea82d1aafc19a6cb7e7a77
Firestar311 <slaterandrew0398@gmail.com>
2025-07-08 16:48:02 -0500

    Removed the EventHandler internal class

--- src/main/java/com/stardevllc/eventbus/impl/SimpleEventBus.java
+++ src/main/java/com/stardevllc/eventbus/impl/SimpleEventBus.java
@@ -4,7 +4,6 @@ import com.stardevllc.eventbus.*;
 import com.stardevllc.helper.ReflectionHelper;
 
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 import java.util.*;
 
 public class SimpleEventBus<T> implements IEventBus<T> {
@@ -124,119 +123,6 @@ public class SimpleEventBus<T> implements IEventBus<T> {
             }
             
             return Integer.compare(o.priority.ordinal(), priority.ordinal());
-        }
-    }
-
-    static class EventHandler<E> {
-        private Object listener;
-        private Map<Class<? extends E>, Method> handlerMethods = new HashMap<>();
-        private EventPriority priority;
-        private boolean ignoreCancelled;
-
-        public EventHandler(Object listener) {
-            this.listener = listener;
-            searchClass(this.listener.getClass());
-        }
-        
-        protected boolean searchForFullClassListener(Class<?> listenerClass) {
-            if (listenerClass.isAnnotationPresent(SubscribeEvent.class)) {
-                return true;
-            }
-            
-            for (Class<?> classInterface : listenerClass.getInterfaces()) {
-                if (classInterface.isAnnotationPresent(SubscribeEvent.class)) {
-                    return true;
-                }
-            }
-            
-            if (!listenerClass.getSuperclass().equals(Object.class)) {
-                return searchForFullClassListener(listenerClass.getSuperclass());
-            }
-            
-            return false;
-        }
-        
-        protected void findAllListenerMethods(boolean fullClassListener, Class<?> listenerClass) {
-            SubscribeEvent fullClassInfo;
-            if (fullClassListener) {
-                fullClassInfo = listenerClass.getAnnotation(SubscribeEvent.class);
-            } else {
-                fullClassInfo = null;
-            }
-            
-            for (Method method : listenerClass.getDeclaredMethods()) {
-                boolean methodListener = method.isAnnotationPresent(SubscribeEvent.class);
-                if (fullClassListener || methodListener) {
-                    SubscribeEvent methodInfo;
-                    if (methodListener) {
-                        methodInfo = method.getAnnotation(SubscribeEvent.class);
-                    } else {
-                        methodInfo = null;
-                    }
-                    
-                    if (fullClassInfo != null) {
-                        this.priority = fullClassInfo.priority();
-                        this.ignoreCancelled = fullClassInfo.ignoreCancelled();
-                    }
-                    
-                    if (methodInfo != null) {
-                        this.priority = methodInfo.priority();
-                        this.ignoreCancelled = methodInfo.ignoreCancelled();
-                    }
-                    
-                    Parameter[] parameters = method.getParameters();
-                    if (parameters.length == 1) {
-                        method.setAccessible(true);
-                        try {
-                            handlerMethods.put((Class<? extends E>) parameters[0].getType(), method);
-                        } catch (Exception e) {}
-                    }
-                }
-            }
-            
-            if (!listenerClass.getSuperclass().equals(Object.class)) {
-                findAllListenerMethods(fullClassListener, listenerClass.getSuperclass());
-            }
-        }
-        
-        protected void searchClass(Class<?> listenerClass) {
-            boolean fullClassListener = searchForFullClassListener(listenerClass);
-            findAllListenerMethods(fullClassListener, listenerClass);
-        }
-
-        public Object getListener() {
-            return listener;
-        }
-
-        public void handle(Object event) {
-            for (Class<?> parameterClazz : handlerMethods.keySet()) {
-                if (parameterClazz.isAssignableFrom(event.getClass())) {
-                    Method method = handlerMethods.get(parameterClazz);
-                    try {
-                        method.invoke(listener, event);
-                    } catch (Exception e) {
-                        throw new RuntimeException("Could not pass " + event.getClass().getName() + " to " + listener.getClass().getName() + "." + method.getName(), e);
-                    }
-                }
-            }
-        }
-
-        @Override
-        public boolean equals(Object object) {
-            if (this == object) {
-                return true;
-            }
-            if (object == null || getClass() != object.getClass()) {
-                return false;
-            }
-
-            EventHandler<?> that = (EventHandler<?>) object;
-            return Objects.equals(listener, that.listener);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(listener);
         }
     }
 }
