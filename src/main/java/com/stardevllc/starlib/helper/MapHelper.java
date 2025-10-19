35e58cf52c8fa3fbeba9b3725c6c542abba49745
Firestar311 <slaterandrew0398@gmail.com>
2025-10-19 13:10:23 -0500

    Made the MapHelper preserve order of the values

--- src/main/java/com/stardevllc/starlib/helper/MapHelper.java
+++ src/main/java/com/stardevllc/starlib/helper/MapHelper.java
@@ -1,7 +1,6 @@
 package com.stardevllc.starlib.helper;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Helper utilities for maps
@@ -21,7 +20,7 @@ public final class MapHelper {
      * @return The created map
      */
     public static <K, V> Map<K, V> of(K firstKey, V firstValue, Object... rawValues) {
-        Map<K, V> map = new HashMap<>();
+        Map<K, V> map = new LinkedHashMap<>();
         map.put(firstKey, firstValue);
         
         if (rawValues != null) {
