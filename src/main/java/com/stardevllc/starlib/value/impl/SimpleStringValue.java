038d9232e9a091ae550e7d2f810dd783bb9acaf6
Firestar311 <slaterandrew0398@gmail.com>
2025-10-19 23:48:05 -0500

    Added constructor overloads for the SimpleStringValue and SimpleUUIDValue

--- src/main/java/com/stardevllc/starlib/value/impl/SimpleStringValue.java
+++ src/main/java/com/stardevllc/starlib/value/impl/SimpleStringValue.java
@@ -3,4 +3,10 @@ package com.stardevllc.starlib.value.impl;
 import com.stardevllc.starlib.value.WritableStringValue;
 
 public class SimpleStringValue extends SimpleObjectValue<String> implements WritableStringValue {
+    public SimpleStringValue() {
+    }
+    
+    public SimpleStringValue(String value) {
+        super(value);
+    }
 }
--- src/main/java/com/stardevllc/starlib/value/impl/SimpleUUIDValue.java
+++ src/main/java/com/stardevllc/starlib/value/impl/SimpleUUIDValue.java
@@ -5,4 +5,10 @@ import com.stardevllc.starlib.value.WritableUUIDValue;
 import java.util.UUID;
 
 public class SimpleUUIDValue extends SimpleObjectValue<UUID> implements WritableUUIDValue {
+    public SimpleUUIDValue() {
+    }
+    
+    public SimpleUUIDValue(UUID value) {
+        super(value);
+    }
 }
