9a4c1d0d07e0f9f42c7debb271186dfe142c7c99
Firestar311 <slaterandrew0398@gmail.com>
2026-04-05 12:50:20 -0500

    Added javadocs to methods in Key

--- src/main/java/com/stardevllc/starlib/objects/key/Key.java
+++ src/main/java/com/stardevllc/starlib/objects/key/Key.java
@@ -8,6 +8,9 @@ import org.jetbrains.annotations.NotNull;
 @FunctionalInterface
 public interface Key extends Comparable<Key> {
     
+    /**
+     * Just an empty key for non-null uses
+     */
     Key EMPTY = new Key() {
         @Override
         public int hashCode() {
@@ -19,6 +22,11 @@ public interface Key extends Comparable<Key> {
             return "EMPTY";
         }
         
+        @Override
+        public boolean isEmpty() {
+            return true;
+        }
+        
         @SuppressWarnings("ComparatorMethodParameterNotUsed")
         @Override
         public int compareTo(@NotNull Key o) {
@@ -26,28 +34,59 @@ public interface Key extends Comparable<Key> {
         }
     };
     
+    /**
+     * Checks to see if this key is empty
+     *
+     * @return If the key is empty
+     */
     default boolean isEmpty() {
         return toString() == null || toString().isBlank();
     }
     
+    /**
+     * Checks to see if this key is not empty
+     *
+     * @return If the key is not empty
+     */
     default boolean isNotEmpty() {
         return !isEmpty();
     }
     
+    /**
+     * Checks to see if this key contains the string
+     *
+     * @param str The string
+     * @return If this key contains the string
+     */
     default boolean contains(String str) {
         return toString().contains(str);
     }
     
+    /**
+     * Checks to see if this key contains the key
+     *
+     * @param key The key
+     * @return If this key contains the key
+     */
     default boolean contains(Key key) {
         return toString().contains(key.toString());
     }
     
+    /**
+     * {@inheritDoc}
+     */
     @Override
     int hashCode();
     
+    /**
+     * {@inheritDoc}
+     */
     @Override
     boolean equals(Object obj);
     
+    /**
+     * {@inheritDoc}
+     */
     @Override
     String toString();
 }
