47ac88613421b617624485860142ca66a2795eb7
Firestar311 <slaterandrew0398@gmail.com>
2026-05-12 19:38:47 -0500

    Made the toArray methods be in the helper as they are pretty much all the same

--- src/main/java/com/stardevllc/starlib/helper/ArrayHelper.java
+++ src/main/java/com/stardevllc/starlib/helper/ArrayHelper.java
@@ -1,10 +1,47 @@
 package com.stardevllc.starlib.helper;
 
-import java.util.Arrays;
-import java.util.Iterator;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public final class ArrayHelper {
     private ArrayHelper() {}
+    
+    public static <E> Object[] toArray(Collection<E> collection) {
+        Object[] r = new Object[collection.size()];
+        Iterator<E> it = collection.iterator();
+        for (int i = 0; i < r.length; i++) {
+            if (!it.hasNext()) {
+                return Arrays.copyOf(r, i);
+            }
+            r[i] = it.next();
+        }
+        return it.hasNext() ? ArrayHelper.finishToArray(r, it) : r;
+    }
+    
+    public static <E, T> T[] toArray(Collection<E> collection, T[] a) {
+        int size = collection.size();
+        T[] r = a.length >= size ? a :
+                (T[]) Array.newInstance(a.getClass().getComponentType(), size);
+        Iterator<E> it = collection.iterator();
+        
+        for (int i = 0; i < r.length; i++) {
+            if (!it.hasNext()) {
+                if (a == r) {
+                    r[i] = null;
+                } else if (a.length < i) {
+                    return Arrays.copyOf(r, i);
+                } else {
+                    System.arraycopy(r, 0, a, 0, i);
+                    if (a.length > i) {
+                        a[i] = null;
+                    }
+                }
+                return a;
+            }
+            r[i] = (T) it.next();
+        }
+        return it.hasNext() ? ArrayHelper.finishToArray(r, it) : r;
+    }
     
     public static <T> T[] finishToArray(T[] r, Iterator<?> it) {
         int len = r.length;
